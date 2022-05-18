ea41d46205375d4b2f5de01357eafd150f1fe92d
difelix <dfelixsilva08@gmail.com>
2022-05-17 21:21:48 -0300

    --commit inicial

---
+++ src/main/kotlin/com/difelix/ecommercesystem/EcommerceSystemApplication.kt
@@ -0,0 +1,11 @@
+package com.difelix.ecommercesystem
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class EcommerceSystemApplication
+
+fun main(args: Array<String>) {
+	runApplication<EcommerceSystemApplication>(*args)
+}
---
+++ src/test/kotlin/com/difelix/ecommercesystem/EcommerceSystemApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.difelix.ecommercesystem
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class EcommerceSystemApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
